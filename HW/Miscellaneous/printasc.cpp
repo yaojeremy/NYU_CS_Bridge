1dfed8179e54823a079e55b2c0ddf2bed848f671
Jeremy Yao <24397363+yaojeremy@users.noreply.github.com>
2019-03-28 18:27:13 +0100

    Update printasc.cpp

--- HW/Miscellaneous/printasc.cpp
+++ HW/Miscellaneous/printasc.cpp
@@ -1,1 +1,30 @@
-#include 
+#include <iostream>
+using namespace std;
+
+void printAsc(int start, int end);
+
+int main(){
+
+  int start, end;
+  
+  cout<<"Please enter two numbers:"<<endl:
+  cin<<start<<end<<endl;
+  
+  printAsc(start,end);
+
+}
+
+
+
+void printAsc(int start, int end){
+
+  if (start == end)
+    cout<<start<<endl;
+
+  else{
+    printAsc(start, end-1);
+    cout<<end<<endl;
+  }
+  
+  
+}
